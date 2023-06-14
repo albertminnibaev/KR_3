fb866e2649c15331b8c303949c127ec39f87e117
Albert <albert.minnibaeff@yandex.ru>
2023-06-15 00:38:02 +0400

    code

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 
+# Импорт функций из файла utils из папки utils
+from utils.utils import get_list_of_recent_operations, get_data, teaching_purpose_of_payment, get_amount
+
+
+def main():
+    """
+    Основной блок программы, выводит информацию о последних пяти оперциях
+    :return: последние пять оперций
+    """
+    for elem in get_list_of_recent_operations():
+        print(f"{get_data(elem)} {elem['description']}")
+        print(f"{teaching_purpose_of_payment(elem)}")
+        print(f"{get_amount(elem)}\n")
+
+
+if __name__ == '__main__':
+    main()
 
